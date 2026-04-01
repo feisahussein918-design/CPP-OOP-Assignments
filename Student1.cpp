8ba78559e3b373504f6a4ed8260e23cb71ff4c8f
feisahussein918-design <faizahasso918@gmail.com>
2026-04-01 22:46:38 +0300

    Create Student1.cpp
    Added student1 program

---
+++ Student1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int roll_no;
+};
+
+int main() {
+    Student s;
+    s.name = "John";
+    s.roll_no = 2;
+
+    cout << "Name: " << s.name << endl;
+    cout << "Roll Number: " << s.roll_no << endl;
+
+    return 0;
+}
